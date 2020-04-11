522105937f42054d414617c953a68eb68141ec44
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-04-11 13:27:48 +0800

    add more tests

--- test/createRequest.test.ts
+++ test/createRequest.test.ts
@@ -2,12 +2,15 @@ import mock, { delay } from 'xhr-mock';
 import { createRequest } from '../src';
 
 describe(`.fetch`, () => {
+  let consoleErrorSpy: jest.MockInstance<any, any> = jest.fn();
+
   beforeEach(() => {
     mock.setup();
   });
 
   afterEach(() => {
     mock.teardown();
+    consoleErrorSpy.mockRestore();
   });
 
   it(`.fetch just like fetch`, async () => {
@@ -52,9 +55,113 @@ describe(`.fetch`, () => {
     expect(onSuccess).toHaveBeenCalledWith(result);
     expect(onError).not.toHaveBeenCalled();
   });
+
+  it(`can be invoked for text`, async () => {
+    const result = 'The request success';
+    mock.get('/api', { status: 200, body: result });
+
+    const onSuccess = jest.fn();
+    const onError = jest.fn();
+
+    await createRequest('/api', {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .fetch()
+      .then(res => res.text())
+      .then(onSuccess, onError);
+
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    expect(onSuccess).toHaveBeenCalledWith(result);
+    expect(onError).not.toHaveBeenCalled();
+  });
+
+  it(`can be invoked for blob`, async () => {
+    expect.assertions(6);
+
+    const result = 'Some text file';
+    mock.post('/api', {
+      status: 200,
+      body: result,
+      headers: {
+        'Content-Length': result.length.toString(),
+        'Content-Type': 'text/plain',
+        'X-custom': 'y',
+      },
+    });
+
+    const onSuccess = jest.fn();
+    const onError = jest.fn();
+
+    await createRequest('/api', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/plain',
+      },
+    })
+      .fetch()
+      .then(res => {
+        expect(res.headers.keys()).toMatchInlineSnapshot(`
+          Array [
+            "content-length",
+            "content-type",
+            "x-custom",
+          ]
+        `);
+        expect(res.headers.entries()).toMatchInlineSnapshot(`
+          Array [
+            Array [
+              "content-length",
+              "14",
+            ],
+            Array [
+              "content-type",
+              "text/plain",
+            ],
+            Array [
+              "x-custom",
+              "y",
+            ],
+          ]
+        `);
+        expect(res.headers.has('Content-Type')).toBe(true);
+        expect(res.headers.get('Content-Type')).toBe('text/plain');
+
+        return res.blob();
+      })
+      .then(onSuccess, onError);
+
+    expect(onSuccess.mock.calls[0][0]).toBeInstanceOf(Blob);
+    expect(onError).not.toHaveBeenCalled();
+  });
+
+  it(`reject Promise when error`, async () => {
+    consoleErrorSpy = jest
+      .spyOn(global.console, 'error')
+      .mockImplementation(() => {});
+
+    mock.get('/api', () => Promise.reject(new Error('Network Error')));
+
+    await expect(
+      createRequest('/api', {
+        headers: {
+          Accept: 'application/json',
+        },
+      }).fetch()
+    ).rejects.toBeDefined();
+  });
 });
 
 describe(`.xhr`, () => {
+  beforeEach(() => {
+    mock.setup();
+  });
+
+  afterEach(() => {
+    mock.teardown();
+  });
+
   it(`can be cancelled`, async () => {
     mock.get('/api', delay({ status: 200 }, 500));
 
